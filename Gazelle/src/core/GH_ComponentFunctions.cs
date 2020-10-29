26696f727757c1189dcecbbf2e74733b61ee2a6e
Jos Feenstra <feenstrajos@gmail.com>
2020-10-29 16:10:34 +0100

    codebase is oficially reanimated: closed breps are being produced, and everything seems to be working as normal. Still, I will keep a close eye on it. We need more unit tests

---
+++ Gazelle/src/core/GH_ComponentFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SferedApi
+{ 
+    internal static class GH_ComponentFunctions
+    {
+        public static List<string> log = new List<string>();
+
+        public static void Print(this GH_Component component, string message)
+        {
+            log.Add(message);
+            component.AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, message);
+        }
+        public static void Warn(this GH_Component component, string message)
+        {
+            log.Add(message);
+            component.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, message);
+        }
+        public static void Error(this GH_Component component, string message)
+        {
+            log.Add(message);
+            component.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, message);
+        }
+    }
+}
